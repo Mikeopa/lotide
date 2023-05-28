29aee414304463db9df007fcbb0f542d3c0c5436
Michael Opadotun <mopadotun.cphomes@gmail.com>
2023-05-27 22:33:19 -0600

    function for finding keys by values

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object && object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  movie: "Disney World",
+  trailers: "Stokes city"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
