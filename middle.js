e44d9e19be0b51dbd3c118bb08a647ddb2b993ba
Michael Opadotun <mopadotun.cphomes@gmail.com>
2023-05-27 20:38:00 -0600

    function for the middle of an array

---
+++ middle.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+  }
+};
+
+const middle = function(arrays) {
+  const length = arrays.length;
+  if (length <= 2) {
+    return [];
+  }
+  const middleindex = Math.floor(length / 2);
+  if (length % 2 === 0) {
+    return [arrays[middleindex - 1], arrays[middleindex]];
+  }
+  if (length % 2 !== 0) {
+    return [arrays[middleindex]];
+  }
+};
+
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
