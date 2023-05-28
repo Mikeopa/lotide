1d4da951f572eb857c6cedce8e85f3d07b1f57b9
Michael Opadotun <mopadotun.cphomes@gmail.com>
2023-05-27 18:52:52 -0600

    array function of arrays

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const flatten = function(array) {
+  let flattenedArray = [];
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      flattenedArray.push(...element);
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray;
+};
+
+flatten(([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
