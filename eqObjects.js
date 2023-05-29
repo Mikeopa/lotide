a941930e10e63b14743c9099683fb1f8df00ab8a
Michael Opadotun <mopadotun.cphomes@gmail.com>
2023-05-28 20:28:25 -0600

    function for comparing array objects

---
+++ eqObjects.js
@@ -0,0 +1,53 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }  return true;
+};
+
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const item1 = Object.keys(object1);
+  const item2 = Object.keys(object2);
+  if (item1.length !== item2.length) {
+    return false;
+  }
+  for (key of item1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (typeof value1 === 'object' && typeof value2 === 'object') {
+      if (!eqObjects(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject);
+
+assertEquals(eqObjects(shirtObject , anotherShirtObject), true);
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject);
+assertEquals(eqObjects(shirtObject , longSleeveShirtObject), false);
